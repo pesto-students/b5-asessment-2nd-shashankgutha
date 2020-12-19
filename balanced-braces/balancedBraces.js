8b58644dc2c06c4479aaa5fdb208582d3beeae45
shashank gutha <shashankgutha@gmail.com>
2020-12-19 17:44:18 +0530

    added braces check function

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,6 +1,44 @@
 
 function balancedBraces(...args) {
-  return args;
+  var output=true;
+  var brackets=['[',']','(',')','{','}'];
+  args.forEach(function(element){
+    var rawelements=element.split("");
+    var elements=[];
+    rawelements.filter(function(e){
+    	if(brackets.includes(e)){
+        	elements.push(e);
+        }
+    });
+    elements.forEach(function(char,i){
+      if(i<(element.length/2)){
+        var oppchar='';
+        if(char==='{'){
+			oppchar='}'
+        }
+        if(char==='('){
+			oppchar=')'
+        }
+        if(char==='['){
+			oppchar=']'
+        }
+        //console.log(elements[elements.length-(i+1)]+' '+i+' '+char);
+        if(oppchar === elements[(i+1)]){
+          console.log(char);
+          elements.splice((i+1), 1);
+      	}else if(oppchar === elements[elements.length-1]){
+          console.log(char);
+          elements.splice(elements.length-1, 1);
+        }else{
+          output=false;
+        }
+      }
+      
+    });
+    
+  });
+
+  return output;
 }
 
 export {
