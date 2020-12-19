6143fb46892f27becdd69e50a87e0c42ca8d1ccb
shashank gutha <shashankgutha@gmail.com>
2020-12-19 17:02:40 +0530

    added cube root

--- array-cube-root-to-json/arrayCubeRootToJson.js
+++ array-cube-root-to-json/arrayCubeRootToJson.js
@@ -1,3 +1,9 @@
-const arrayCubeRootToJson = arr => {};
+const arrayCubeRootToJson = arr => {
+  var output={};
+  arr.forEach(function(element){
+    output[`'${element}'`]=Math.cbrt(element);
+  });
+  return output
+  };
 
 export { arrayCubeRootToJson };
