d0fe6df73550bf5fec17b9fa22dc4bb1aa624eef
shashank gutha <shashankgutha@gmail.com>
2020-12-22 16:35:12 +0530

    braces function enhanced

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,41 +1,51 @@
-
 function balancedBraces(...args) {
   var output=true;
   var brackets=['[',']','(',')','{','}'];
   args.forEach(function(element){
     var rawelements=element.split("");
     var elements=[];
+   
     rawelements.filter(function(e){
     	if(brackets.includes(e)){
         	elements.push(e);
         }
     });
-    elements.forEach(function(char,i){
-      if(i<(element.length/2)){
-        var oppchar='';
-        if(char==='{'){
-			oppchar='}'
-        }
-        if(char==='('){
-			oppchar=')'
-        }
-        if(char==='['){
-			oppchar=']'
-        }
-        //console.log(elements[elements.length-(i+1)]+' '+i+' '+char);
-        if(oppchar === elements[(i+1)]){
-          console.log(char);
-          elements.splice((i+1), 1);
-      	}else if(oppchar === elements[elements.length-1]){
-          console.log(char);
-          elements.splice(elements.length-1, 1);
-        }else{
-          output=false;
-        }
-      }
-      
-    });
     
+     //console.log(elements);
+    
+    for(var i=0;i<elements.length;i++){
+ 		  var ori=i;
+ 		  var j=1;
+  		  var compare=i+j;
+          var oppchar='';
+      	  var char=elements[ori];
+          if(char==='{'){
+              oppchar='}';
+          }
+          if(char==='('){
+              oppchar=')';
+          }
+          if(char==='['){
+              oppchar=']';
+          }
+		   while(compare<=elements.length){
+           if(oppchar === elements[compare]){
+          		//console.log(char);
+          		elements.splice(compare, 1);
+             	output=true;
+             	break;
+      		}else{
+              output=false;
+            }
+    		//console.log(i+' compared with '+compare+' '+output)
+    		j+=2
+    		compare=(i)+j;
+  			}
+			
+      		if(!output){
+              break;
+            }    		
+    }
   });
 
   return output;
