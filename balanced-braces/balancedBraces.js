ab782ef7d5d7b7b7b3e790b063986df0db80d0c2
shashank gutha <shashankgutha@gmail.com>
2020-12-22 16:38:08 +0530

    braces function enhanced

--- balanced-braces/balancedBraces.js
+++ balanced-braces/balancedBraces.js
@@ -1,56 +1,56 @@
 function balancedBraces(...args) {
-  var output=true;
-  var brackets=['[',']','(',')','{','}'];
-  args.forEach(function(element){
-    var rawelements=element.split("");
-    var elements=[];
-   
-    rawelements.filter(function(e){
-    	if(brackets.includes(e)){
-        	elements.push(e);
+    var output = true;
+    var brackets = ['[', ']', '(', ')', '{', '}'];
+    args.forEach(function(element) {
+        var rawelements = element.split("");
+        var elements = [];
+
+        rawelements.filter(function(e) {
+            if (brackets.includes(e)) {
+                elements.push(e);
+            }
+        });
+
+        //console.log(elements);
+
+        for (var i = 0; i < elements.length; i++) {
+            var ori = i;
+            var j = 1;
+            var compare = i + j;
+            var oppchar = '';
+            var char = elements[ori];
+            if (char === '{') {
+                oppchar = '}';
+            }
+            if (char === '(') {
+                oppchar = ')';
+            }
+            if (char === '[') {
+                oppchar = ']';
+            }
+            while (compare <= elements.length) {
+                if (oppchar === elements[compare]) {
+                    //console.log(char);
+                    elements.splice(compare, 1);
+                    output = true;
+                    break;
+                } else {
+                    output = false;
+                }
+                //console.log(i+' compared with '+compare+' '+output)
+                j += 2
+                compare = (i) + j;
+            }
+
+            if (!output) {
+                break;
+            }
         }
     });
-    
-     //console.log(elements);
-    
-    for(var i=0;i<elements.length;i++){
- 		  var ori=i;
- 		  var j=1;
-  		  var compare=i+j;
-          var oppchar='';
-      	  var char=elements[ori];
-          if(char==='{'){
-              oppchar='}';
-          }
-          if(char==='('){
-              oppchar=')';
-          }
-          if(char==='['){
-              oppchar=']';
-          }
-		   while(compare<=elements.length){
-           if(oppchar === elements[compare]){
-          		//console.log(char);
-          		elements.splice(compare, 1);
-             	output=true;
-             	break;
-      		}else{
-              output=false;
-            }
-    		//console.log(i+' compared with '+compare+' '+output)
-    		j+=2
-    		compare=(i)+j;
-  			}
-			
-      		if(!output){
-              break;
-            }    		
-    }
-  });
 
-  return output;
+    return output;
 }
 
 export {
-  balancedBraces,
+    balancedBraces,
 };
